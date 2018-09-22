cf5e9a96d0870484a654201c4d8883aa4605a266
lukej <lukexjohnson@outlook.com>
2018-09-23 09:21:42 +1000

    testing booking entities

--- test/hotel/entities/TestBooking.java
+++ test/hotel/entities/TestBooking.java
@@ -25,19 +25,25 @@ import static org.mockito.ArgumentMatchers.anyInt;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @ExtendWith(MockitoExtension.class)
 class TestBooking {
 	
 	Booking booking;
-	Room room;
+	//@Mock Room room;
 	@Mock Guest guest;
 	@Mock CreditCard creditCard;
 	SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
-
-		
+	//@Spy Map<Long, Booking> bookingsByConfirmationNumber = new HashMap<Long, Booking>();
+    @Spy ArrayList<Booking> bookings;
+	
+    @InjectMocks Room room = new Room(99, RoomType.SINGLE);
+    
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
 	}
@@ -54,8 +60,9 @@ class TestBooking {
 		
 		Date date = format.parse("01-01-2018");
 		
-		room = new Room(101, RoomType.SINGLE);
+		//room = new Room(101, RoomType.SINGLE);
 		booking = new Booking(guest, room, date, 0, 0, creditCard);
+		
 	}
 
 	
@@ -65,18 +72,67 @@ class TestBooking {
 
 	
 	@Test
-	void testCheckinRoomAvailable() {
-		
+	void testCheckinTwiceShouldThrowException() {		
 		//arrange		
-		assertTrue(booking.isPending());
-		
-		//act	
 		booking.checkIn();
-		
+				
+		//act
+		Executable executable = () -> booking.checkIn();		
+				
 		//assert
-		assertTrue(booking.isCheckedIn());
+		assertThrows(RuntimeException.class, executable);
 		
-	}		
-
+	}
+	
+	@Test
+	void testSuccessfulCheckInShouldUpdateStateToOccupied() {	
+		//arrange		
+						
+		//act
+		booking.checkIn();		
+			
+		//assert
+		assertTrue(room.isOccupied());
+		assertTrue(booking.isCheckedIn());
+	}
+	
+	@Test
+	void testAddServiceChargeSuccessful() {		
+		//arrange
+		booking.checkIn();
+		ServiceType barFridge = ServiceType.BAR_FRIDGE;
+				
+		//act
+		booking.addServiceCharge(barFridge, 7.99);	
+				
+		//assert
+		assertEquals(barFridge, booking.getCharges().get(0).getType());
+	}
+	
+	@Test
+	void testCheckOutWhenNotCheckedInRuntimeException() {		
+		//arrange				
+				
+		//act
+		Executable executable = () -> booking.checkOut();		
+				
+		//assert
+		assertThrows(RuntimeException.class, executable);
+		
+	}
+	
+	@Test
+	void testSuccessfulCheckOut() {	
+		//arrange
+		bookings.add(booking);
+		booking.checkIn();				
+		
+		//act
+		booking.checkOut();		
+			
+		//assert
+		assertTrue(room.isReady());
+		assertTrue(booking.isCheckedOut());
+	}
 
 }
